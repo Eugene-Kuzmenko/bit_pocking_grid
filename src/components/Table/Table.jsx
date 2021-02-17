d0fca4380b2af8bf843ea0f0f97afa5fcc90d15f
Eugene <42930224+Eugene-Kuzmenko@users.noreply.github.com>
2021-02-17 09:39:16 +0300

    Add grid notation

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,6 +3,8 @@ import clsx from "clsx";
 import Cell from "../Cell/Cell";
 import styles from "./Table.module.scss";
 
+const RANK = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
 function Table({ numRows, numColumns }) {
   const rows = [];
   for (let i = 0; i < numRows; i++) {
@@ -12,11 +14,23 @@ function Table({ numRows, numColumns }) {
     }
     rows.push(
       <div key={i} className={styles.row}>
+        <div className={styles.notation}>{8 - i}</div>
         {columns}
       </div>
     );
   }
-  return <div className={styles.root}>{rows}</div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.titles}>
+        {RANK.map((key) => (
+          <div key={key} className={styles.notation}>
+            {key}
+          </div>
+        ))}
+      </div>
+      {rows}
+    </div>
+  );
 }
 
 export default memo(Table);
